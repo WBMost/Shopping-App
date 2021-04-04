549aabb28754f264aaa3f9142848c032fec2732f
Wyatt Mosteller <WBMosteller@student.fullsail.edu>
2021-04-04 19:08:53 -0400

    Added buttons and basic login to app

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -4,12 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
-    boolean passHide;
+    boolean passHide = true;
+    Button login;
+    EditText User,Pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button)findViewById(R.id.login);
+        User = (EditText)findViewById(R.id.Username);
+        Pass = (EditText)findViewById(R.id.Password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(User.getText().toString().equals("admin") && Pass.getText().toString().equals("admin"))
+                    Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        );
     }
 }
